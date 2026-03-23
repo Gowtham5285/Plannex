ad702403b746f1255db0f35cf7a8fae97dfb234d
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2026-03-23 09:26:41 +0530

    added the jwt token auth

--- server/app.js
+++ server/app.js
@@ -1,26 +1,29 @@
-const express = require("express")
+const express = require("express");
 const app = express()
-const { port } = require("./constants/env.js")
+const {port} = require("./constants/env.js")
+const {apiNotFound, globalError} = require("./middlewares/appLevelMiddlewares.js")
+const { connectDb } = require("./configs/mongodb.js")
+connectDb()
 
-const { apiNotFound,globalError} = require("./middlewares/appLevelMiddlewares.js")
+app.use(express.json())
+app.use(express.urlencoded())
 
-
-app.get("/", (req, res) => {
+app.get("/", (req, res)=>{
     try {
-        res.send("i am healthy port")
+        res.status(200).send("Im running healthy")
     } catch (error) {
         console.log(error)
-        res.send("something went wrong")
+        res.status(400).send("something went wrong")
     }
 })
 
 app.use("/api/auth", require("./routes/auth.js"))
-app.use("/api/users",require("./routes/admin.js"))
-app.use("/api/events",require("./routes/events.js"))
-app.use("/api/eventRegister",require("./routes/eventRegistration.js"))
-app.use("/api/upload",require("./routes/fileupload.js"))
+app.use("/api/users", require("./routes/admin.js"))
+app.use("/api/events", require("./routes/events.js"))
+app.use("/api/eventRegister", require("./routes/eventRegistration.js"))
+app.use("/api/uploads", require("./routes/fileupload.js"))
 
 app.use(apiNotFound)
 app.use(globalError)
 
-app.listen(port, () => { console.log("Server started on port " + port) })
+app.listen(port,()=>{console.log("server started on port "+port)})
--- server/middlewares/appLevelMiddlewares.js
+++ server/middlewares/appLevelMiddlewares.js
@@ -1,7 +1,11 @@
-exports.apiNotFound = (req, res, next) => {
-    res.status(404).json({ endpoint: req.method + " " + req.url, message: "API not found" })
+exports.apiNotFound=(req, res, next)=>{
+    res.status(404).json({endpoint: req.method+ " " + req.url  ,message: "api not found"})
 }
 
-exports.globalError = (error, req, res, next) => {
-    res.status(400).json({ message: "something went wrong", error_code: error.message})
+exports.globalError=(error, req, res, next)=>{
+    console.log(error)
+    return res.status(error.statusCode).json({success:false, message:error.message || "something went wrong", error:{
+        code: error.message.toUpperCase() || "SOMETHING_WENT_WRONG",
+        data:error.message
+    }})
 }
